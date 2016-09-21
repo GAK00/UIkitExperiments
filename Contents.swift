2a7b2ec41a323176cbc56bb5e3d2c8cc5e70ccaf
GAK00 <geran.kunz@gmail.com>
2016-09-21 08:12:07 -0600

    Reuploaded UIkit testing with button and circles and other fun stuff

---
+++ Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var str = "Hello, playground"
+//view 1
+var DynamicView=UIView(frame: CGRectMake(100,200,100,100))
+DynamicView.backgroundColor=UIColor.blueColor()
+DynamicView.layer.cornerRadius = 20;
+DynamicView.layer.borderWidth=4
+//button 1
+var button  = UIButton(frame:CGRectMake(50,100,50,50))
+button.backgroundColor = UIColor.brownColor()
+button.layer.borderWidth=4
+var labeltest = UILabel(frame: CGRectMake(100,100,100,100))
+labeltest.text = "hello"
+labeltest
+var circleattempt = UIView(frame: CGRectMake(100,100,100,100))
+circleattempt.layer.cornerRadius = 50
+circleattempt.backgroundColor = UIColor.redColor()
+circleattempt
+var lowCornerRadius = UIView(frame: CGRectMake(100,1000,1000,100))
+lowCornerRadius.layer.cornerRadius = 1000
+lowCornerRadius.layer.borderWidth = 25
+lowCornerRadius.backgroundColor = UIColor.purpleColor()
+lowCornerRadius
+var YourDumb = UILabel(frame: CGRectMake(100,100,200,200))
+YourDumb.text = "Your Dumb"
+YourDumb.textColor = UIColor.yellowColor()
+YourDumb
+var Strangebutton = UIButton(frame: CGRectMake(100,100,100,100))
+Strangebutton.centerXAnchor
+Strangebutton.backgroundColor = UIColor.whiteColor()
+Strangebutton
+
+var sketchDatePickerThing = UIDatePicker(frame: CGRectMake(100,100,1000,1000))
+sketchDatePickerThing
+
+
+
+
+
+
+button
+DynamicView
+
+
+
+
